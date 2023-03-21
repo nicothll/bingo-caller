bc2ab65bfe0d7c90840d0e3c53ca1268dcdbe903
Nicolas Thuillier <nicothll.dev@gmail.com>
2023-03-20 19:39:50 -0500

    Added Pipfiles and updated README.md and main.py

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 ### With Python and Streamlit
 
 <br>
+
 ## Run App
 To run the Streamlit application execute:
 ```python
 streamlit run main.py
 ```
+
 <br> 
--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import app
-import bingo
 
 
 def main():
-    app.run_app()
-    # print("NEW GAME")
-    # game = bingo.BingoGame()
-    # while True:
-    #     user = input("Press enter to call, save to save: > ")
-    #     if user == "":
-    #         number = game.call()
-    #         print(number)
-    #     elif user.lower() == "save":
-    #         game.save_game()
-    #     else:
-    #         break
+    """Run streamlit application"""
 
-    # print("GAME OVER")
+    app.run_app()
 
 
 if __name__ == "__main__":
